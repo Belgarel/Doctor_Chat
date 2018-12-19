40f604be9d9bd36a488d7d2e949247cbfe81f247
Julien Mathieu <44834160+yaki82@users.noreply.github.com>
2018-12-19 09:11:44 +0100

    Add files via upload

--- Creation_BDD_DRC.sql
+++ Creation_BDD_DRC.sql
@@ -49,46 +49,12 @@ alter table DRC_MESSAGE
    add constraint DRC_MESSAGE_NO_CONVERS_fk foreign key (NO_CONVERSATION)
       references DRC_CONVERSATION;
 
-/*Setting up the primary key for drc_utilisateur to auto increment*/
-create sequence drc_utilisateur_pk_seq start with 1;
-create or replace trigger drc_utilisateur_pk_autoincr
-before insert on drc_utilisateur for each row
-begin
-    select drc_utilisateur_pk_seq.nextval
-    into :new.no_utilisateur
-    from dual;
-end;
+ALTER TABLE drc_utilisateur ADD CONSTRAINT un_login UNIQUE (login);
 
-/*Inserting values into DRC_UTILISATEUR*/
-insert into drc_utilisateur (login, password)
-values ('test', '123');
-insert into drc_utilisateur (login, password)
-values ('victor hugo', '123');
-insert into drc_utilisateur (login, password)
-values ('gustave flaubert', '123');
-insert into drc_utilisateur (login, password)
-values ('jules verne', '123');
-insert into drc_utilisateur (login, password)
-values ('marcel proust', '123');
-insert into drc_utilisateur (login, password)
-values ('stanislas lem', '123');
+ALTER TABLE drc_contact
+RENAME COLUMN no_utilisateur_1 TO no_utilisateur;
 
-/*Inserting values into DRC_CONTACT*/
-insert into drc_contact
-values (1,6);
-insert into drc_contact
-values (2,6);
-insert into drc_contact
-values (3,6);
-insert into drc_contact
-values (4,6);
-insert into drc_contact
-values (5,6);
-insert into drc_contact
-values (1,2);
-insert into drc_contact
-values (3,2);
-insert into drc_contact
-values (4,2);
-insert into drc_contact
-values (5,2);
+ALTER TABLE drc_contact
+RENAME COLUMN no_utilisateur_2 TO no_contact;
+
+COMMIT;
