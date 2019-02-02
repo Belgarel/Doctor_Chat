f67f7697cb49d4d616acc85679432ee063db775e
Julien Mathieu <44834160+yaki82@users.noreply.github.com>
2019-02-02 18:48:36 +0100

    Ajouter un Contact (en cours)

--- Doctor_Chat/src/doctor_chat/client/AddContactViewController.java
+++ Doctor_Chat/src/doctor_chat/client/AddContactViewController.java
@@ -5,6 +5,13 @@
  */
 package doctor_chat.client;
 
+import doctor_chat.client.connection.Client;
+import doctor_chat.client.connection.ConnectionNotInitializedException;
+import doctor_chat.common.User;
+import doctor_chat.common.connection.ContactRequest;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -15,21 +22,51 @@ import javafx.scene.control.TextField;
  * @author Home
  */
 public class AddContactViewController {
-
+    
     @FXML
     private TextField login;
     @FXML
     private Label errorLabel;
 
     @FXML
-    private void send(ActionEvent event) {
+    private void send(ActionEvent event) throws ConnectionNotInitializedException {
+       
+        String contactLogin = login.getText();
+        
+        //Vérifier que quelque chose a été renvoyé.
+        if (contactLogin == null || "".equals(contactLogin))
+            return;
+        //Vérifier que le login ne fait pas déjà partie des contacts.
+        boolean found = false;
+        String login = "";
+        
+        for (User u : ViewController.instance().getContacts())
+        {
+            login = u.getLogin();
+            found = contactLogin.equals(login);
+            System.out.println(found);
+            if (found) {
+                this.showError("Erreur : " + contactLogin + " fait déjà partie de vos contacts.");
+                return;
+            }
+            try {
+                Client.instance().sendMessage(new ContactRequest(ViewController.instance().getAccount(), contactLogin));
+            } catch (ConnectionNotInitializedException ex) {
+                Logger.getLogger(ChatViewController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 
     @FXML
-    private void cancel(ActionEvent event) {
+    private void cancel(ActionEvent event) {  
     }
     
     public void showError(String err) {
-        
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                errorLabel.setText(err);
+            }
+        });
     }
 }
